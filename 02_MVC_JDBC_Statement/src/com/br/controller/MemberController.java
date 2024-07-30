cf50a52324513ccafae50cd444aaeab48b88765e
misun333 <zxc33387@naver.com>
2024-07-30 19:00:44 +0900

    Update MemberController.java

--- 02_MVC_JDBC_Statement/src/com/br/controller/MemberController.java
+++ 02_MVC_JDBC_Statement/src/com/br/controller/MemberController.java
@@ -15,6 +15,8 @@ import com.br.view.MemberView;
  */
 public class MemberController {
 	
+	//MemberView mv = new MemberView();
+	
 	public void selectMemberList() {
 		List<Member> list = new MemberDao().selectMemberList();
 		
@@ -37,9 +39,71 @@ public class MemberController {
 		// 매개변수 생성자 이용
 		Member m = new Member(userId, userPwd, userName, gender, Integer.parseInt(age), email, phone, hobby);
 		
-		new MemberDao().insertMember(m);
+		int result = new MemberDao().insertMember(m);
+		
+		// 결과를 가지고 성공/실패 판단 후 사용자가 보게될 응답화면지정
+		if(result > 0) { // 성공
+			new MemberView().displaySuccess("성공적으로 회원 추가되었습니다.");
+		}else { // 실패
+			new MemberView().displayFail("회원 추가를 실패했습니다..");
+		}
 		
 	}
+	
+	public void selectMemberByUserId(String userId) {
+		Member m = new MemberDao().selectMemberByUserId(userId);
+		
+		if(m == null) { // 조회결과없음
+			new MemberView().displayNoData(userId + "에 대한 검색 결과가 없습니다.");
+		}else { // 조회결과있음
+			new MemberView().displayMemberData(m);
+		}
+	}
+	
+	public void selectMemberByUserName(String userName) {
+		List<Member> list = new MemberDao().selectMemberByUserName(userName);
+	
+		if(list.isEmpty()) { // 조회결과x
+			new MemberView().displayNoData(userName + "에 대한 검색 결과가 없습니다.");
+		}else { // 조회결과o
+			new MemberView().displayMemberListData(list);
+		}
+	}
+	
+	public void updateMember(String userId, String userPwd
+						   , String email, String phone, String hobby) {
+		
+		Member m = new Member();
+		m.setUserId(userId);
+		m.setUserPwd(userPwd);
+		m.setEmail(email);
+		m.setPhone(phone);
+		m.setHobby(hobby);
+		
+		int result = new MemberDao().updateMember(m);
+		
+		if(result > 0) {
+			new MemberView().displaySuccess("성공적으로 정보 변경 되었습니다.");
+		}else {
+			new MemberView().displayFail("정보 변경에 실패했습니다.");
+		}
+		
+		
+	}
+	
+	public void deleteMember(String userId) {
+		int result = new MemberDao().deleteMember(userId);
+		
+		if(result > 0) {
+			new MemberView().displaySuccess("성공적으로 탈퇴 처리 되었습니다.");
+		}else {
+			new MemberView().displayFail("탈퇴 처리에 실패했습니다.");
+		}
+	}
+	
+	
+	
+	
 	
 	
 	
